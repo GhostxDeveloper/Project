305313177d07084ad1a4a0a2684fb6b9733179a3
cris <2022371192@uteq.edu.mx>
2025-03-05 21:43:21 -0600

    frontend

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = "http://localhost:3000";
+const API_URL = "https://backend-vch5.onrender.com";
 
 // Auth Services
 export const login = async (email, password) => {
